7c1d910cb550d9d231ed694dc6971c6cbe8b0b20
Michael Lulich <michael.lulich@gmail.com>
2018-07-01 11:59:03 -0500

    added _contact partial access

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  def friendly_updated_at
+    updated_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    full_name = first_name + " " + last_name
+  end
+
 end
